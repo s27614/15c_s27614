1bdbf8244a630b0c354a5792320d99c52a3209dc
s27614 <126959703+s27614@users.noreply.github.com>
2024-03-24 12:07:43 +0100

    Revert "Lab 3"

--- square_generator/square_generator.py
+++
@@ -1,7 +0,0 @@
-class SquareGenerator:
-    def generate_squares(self, start, end):
-        if end < start:
-            raise ValueError("End of the range must be greater than or equal to the start.")
-
-        squares = [x ** 2 for x in range(start, end + 1)]
-        return squares
